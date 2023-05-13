4f4c040b9a6c5b06834965ca9869594f3d25b237
Audrey Zhu <audreyzhu059@gmail.com>
2023-05-13 14:39:00 -0700

    commiting to main owo

--- calendar_event.py
+++ calendar_event.py
@@ -11,7 +11,29 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-def create_events(events):
+
+def convert_json(event):
+    event_name = event["name"]["value"]
+    location = event["location"]["value"]
+    start = event["start"]["value"]
+    end = event["end"]["value"]
+
+    cal_event = {
+        'summary' : f'Free Food at {event_name}',
+        'location' : '',
+        'start' : {
+            'dateTime': start, # YYYY-MM-DDThh:mm:ss+00:00
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end' : {
+            'dateTime': end,
+            'timeZone': 'America/Los_Angeles',
+        }
+    }
+    
+    return cal_event
+
+def create_events(new_event, max_events = 10, days_ago=10):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
@@ -38,22 +60,19 @@ def create_events(events):
 
         # Call the Calendar API
         now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        min_day = (datetime.datetime.now() - datetime.timedelta(days = days_ago)).isoformat() + 'Z'
+        print(min_day)
         print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
+        events_result = service.events().list(calendarId='primary', timeMin=min_day,
+                                              maxResults=max_events, singleEvents=True,
                                               orderBy='startTime').execute()
         event_list = events_result.get('items', [])
-        event_names = [event_list[i]['summary'] for i in range(len(event_list))]
+        event_name = [event_list[i]['summary'] for i in range(len(event_list))]
         # create events from events
 
-        for e in events:
-            if e['summary'] not in event_names:
-                event = service.events().insert(calendarId='primary', body=e).execute()
-                print('Event created: %s' % (event.get('htmlLink')))
-
-        if not events:
-            print('No upcoming events found.')
-            return
+        if new_event['summary'] not in event_name:
+            event = service.events().insert(calendarId='primary', body=new_event).execute()
+            print('Event created: %s' % (event.get('htmlLink')))
 
         # Prints the start and name of the next 10 events
         for event in event_list:
@@ -65,15 +84,15 @@ def create_events(events):
 
 
 events = [{
-    'summary': f'[Free Food] pizza at lmao!',
+    'summary': f'[Free Food] pizza at lmao!!',
     'location': '',
     'description': f'There will be free food at ',
     'start': { 
-        'dateTime': "2023-05-28T07:00:00", # YYYY-MM-DDThh:mm:ss+00:00
+        'dateTime': "2023-05-12T13:00:00.000Z", # YYYY-MM-DDThh:mm:ss+00:00
         'timeZone': 'America/Los_Angeles',
     },
     'end': {
-        'dateTime': "2023-05-28T09:00:00",
+        'dateTime': "2023-05-12T15:00:00.000Z",
         'timeZone': 'America/Los_Angeles',
     },
     },{
@@ -104,4 +123,28 @@ events = [{
 
 
 if __name__ == '__main__':
-    create_events(events)
+    event_json = {
+        "name": {
+            "value": "Culture Showcase",
+            "source": "Culture Showcase",
+            "confidence": 0.9
+        },
+        "start": {
+            "value": "2023-05-12T13:00:00.000",
+            "source": "Friday, May 12th from 1-4pm",
+            "confidence": 0.8
+        },
+
+        "end": {
+            "value": "2023-05-12T15:00:00.000",
+            "source": "Friday, May 12th from 1-4pm",
+            "confidence": 0.8
+        },
+        "location": {
+            "value": "Storke Plaza",
+            "source": "Storke Plaza",
+            "confidence": 0.9
+        }
+    }
+    e = convert_json(event_json)
+    create_events(e)
